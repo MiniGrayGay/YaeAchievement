602cf06a8b2aa3bbc1c5ff516d4dda0eca53a812
HolographicHat <holographichat@outlook.com>
2025-03-26 16:03:26 +0800

    bump version

--- YaeAchievement/src/GlobalVars.cs
+++ YaeAchievement/src/GlobalVars.cs
@@ -21,8 +21,8 @@ public static class GlobalVars {
     public static readonly string CachePath = Path.Combine(DataPath, "cache");
     public static readonly string LibFilePath = Path.Combine(DataPath, "YaeAchievement.dll");
 
-    public const uint   AppVersionCode = 234;
-    public const string AppVersionName = "5.2";
+    public const uint   AppVersionCode = 235;
+    public const string AppVersionName = "5.3";
 
     public const string PipeName = "YaeAchievementPipe";
     public const string RinBucketHost = "https://rin.holohat.work";
